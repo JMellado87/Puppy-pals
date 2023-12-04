c70fa9d68dbe8db82eeb51433fef7e31eb850ee6
jonas <jonasabelmellado@gmail.com>
2023-12-04 14:54:53 -0500

    inital commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,17 @@
-
 import './App.css'
+import {puppyList} from './data.js'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  console.log(puppyList)
   return (
     <>
       <div>
+      
       </div>
+
     </>
   )
 }
